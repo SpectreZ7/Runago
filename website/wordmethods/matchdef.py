c28778ee6c7ed3b28e10eb33423a8d9fb39359da
Spectre <iaincorreia@gmail.com>
2023-08-01 06:18:39 +0530

    working on backend matching words

--- website/wordmethods/matchdef.py
+++ website/wordmethods/matchdef.py
@@ -11,17 +11,17 @@ def_word = {
        }
 
 word_defs = matchdef(def_word)
-def checkword(shuffled_words):
+def checkword(word, shuffled_words):
     for i in range(len(shuffled_words)):
-        if shuffled_words[i] == word_defs[i][1]:
+        if shuffled_words[i] == word:
             return i
-def checkdef(shuffled_definitions):
+def checkdef(definition, shuffled_definitions):
     for i in range(len(shuffled_definitions)):
-        if shuffled_definitions[i] == word_defs[i][0]:
+        if shuffled_definitions[i] == definition:
             return i
 shuffled_words = ["king", "glass", "brave", "idiot", "pathetic", "desk"]
 shuffled_definitions = ["lmea ahconrm fo a ajomr itteloarirr tnui", "or siwgonh nmelta or lmrao tstrengh to caef rngdae arfe or ifytifclud haingv or hgoswin uoegarc erguoca", "oflhosi ro pdusit rponse", "a pciycaat to vemo noe ot erthie ocmopaeisnsat nspmcoaotasei ro ouptecnsmtuo tiyp", "altbe frema or acse hwti a songilp or ohoirtnazl asercuf lcaiyeepls fro gtriniw dna eanrdgi nda oetfn htwi arsdwre terocstpmamn nad ihooegneslp"]
-testword = checkword(shuffled_words)
-testdef = checkdef(shuffled_definitions)
-print(testword)
-print(testdef)
+testword = checkword("pathetic",shuffled_words)
+testdef = checkdef("or siwgonh nmelta or lmrao tstrengh to caef rngdae arfe or ifytifclud haingv or hgoswin uoegarc erguoca",shuffled_definitions)
+def check_correct(string):
+    if string == 
